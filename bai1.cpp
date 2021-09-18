e6c9fdcee0c94764d90a28314e80fb371d9c5b1b
Tung1227 <Tungluong2706@gmail.com>
2021-09-18 10:29:15 +0700

    first commit

--- bai1.cpp
+++ bai1.cpp
@@ -4,39 +4,7 @@
 #include <algorithm>
 using namespace std;
 
-void quickSort(int a[], int l,int r){
-    int p = a[(l+r)/2];
-    int i = l, j = r;
-    while(i<j){
-        while(a[i] < p){
-            i++;
-        }
-        while(a[j] > p){
-            j--;
-        }
-        if(i<=j){
-            int temp = a[i];
-            a[i] = a[j];
-            a[j] = temp;
-            i++;
-            j--;
-        }
-        if(i < r) quickSort(a,i,r);
-        if(l < j) quickSort(a,l,j);
-    }
-}
-void solve(int a[], int n){
-    for(int i = 0; i < n; i++){
-        if(a[i] < 0){
-            a[i]*=-1;
-        }
-    }
-    quickSort(a,0,n-1);
-    for(int i = 0; i < n; i++){
-        a[i] = a[i]*a[i];
-        cout << a[i] << " ";
-    }
-}
+
 int main(){
     std::vector<int> vec;
     std::string buffer;
---
+++ bai2.py
@@ -0,0 +1,7 @@
+class Coin{
+    demon: number;
+    quantity: number;
+};
+def coinChange(coins: Coin[], amount: number):number{
+    Coin[] = input()
+}
